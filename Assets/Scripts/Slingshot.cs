89bade4ed81977825da4b351fdd7305eb86b8b9c
cms638 <47014511+cms638@users.noreply.github.com>
2019-02-19 00:00:42 -0500

    Line and Goal

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -12,6 +13,13 @@ public class Slingshot : MonoBehaviour {
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
+
+    static public Vector3 LAUNCH_POS {
+        get {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
 
     void Awake()
     {
